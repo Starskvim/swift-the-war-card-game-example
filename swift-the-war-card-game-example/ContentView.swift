44224c9085fe4dc3b17342949aaa57917b0d0fe7
starskvim <starskvim@mail.ru>
2023-11-05 13:55:55 +0300

    Initial Commit

---
+++ swift-the-war-card-game-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-the-war-card-game-example
+//
+//  Created by Михаил Гайволя on 05.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-the-war-card-game-example/swift_the_war_card_game_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_the_war_card_game_exampleApp.swift
+//  swift-the-war-card-game-example
+//
+//  Created by Михаил Гайволя on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_the_war_card_game_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
